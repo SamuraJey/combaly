f7cae8145ea85c2a7dbe790573792ddbf6e4f408
SamuraJey <cergeypro703@gmail.com>
2024-11-04 13:44:51 +0500

    Refactor lab_b.py: Add type hints, separate input/output functions, and implement main function

--- second_term/B. lab 2 autumn/lab_b.py
+++ second_term/B. lab 2 autumn/lab_b.py
@@ -1,9 +1,9 @@
-def find_distinct_representatives(sets):
+def find_distinct_representatives(sets: list) -> tuple:
     n = len(sets)
     representatives = [0] * n
     used = set()
 
-    def backtrack(index):
+    def backtrack(index: int) -> bool:
         if index == n:
             return True
 
@@ -22,18 +22,34 @@ def find_distinct_representatives(sets):
     else:
         return 'N',
 
-with open('in.txt', 'r') as file:
-    sets_input = []
-    n = int(file.readline().strip())
-    for _ in range(n):
-        line = list(map(int, file.readline().strip().split()))
-        sets_input.append(line[:-1])  # Remove 0 at the end of each line
 
-result = find_distinct_representatives(sets_input)
+def read_input(file_path: str) -> list:
+    with open(file_path, 'r') as file:
+        inputs = []
+        n = int(file.readline().strip())
+        for _ in range(n):
+            # We don't need to convert the elements to integers, because we don't use them as integers
+            # using strings expands the program's capabilities
+            line = list(file.readline().strip().split())
+            # line = list(map(int, file.readline().strip().split()))
+            inputs.append(line[:-1])  # Remove 0 at the end of each line
+        return inputs
 
-with open('out.txt', 'w') as file:
-    if result[0] == 'Y':
-        file.write('Y\n')
-        file.write(' '.join(map(str, result[1])) + '\n')
-    else:
-        file.write('N\n')
+
+def write_output(file_path: str, result: tuple) -> None:
+    with open(file_path, 'w') as file:
+        if result[0] == 'Y':
+            file.write('Y\n')
+            file.write(' '.join(map(str, result[1])) + '\n')
+        else:
+            file.write('N\n')
+
+
+def main():
+    inputs = read_input('in.txt')
+    result = find_distinct_representatives(inputs)
+    write_output('out.txt', result)
+
+
+if __name__ == '__main__':
+    main()
